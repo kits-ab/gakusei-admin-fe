84693abba5f35a1a613103fe19d8170d9535aae6
Doverstav <pontus.doverstav@kits.se>
2017-09-26 16:35:00 +0200

    Import userService into userScreen

--- src/main/js/screens/app/screens/users/userScreen.js
+++ src/main/js/screens/app/screens/users/userScreen.js
@@ -3,6 +3,8 @@ import { Grid, Row, Col, ListGroup, ListGroupItem, Panel } from 'react-bootstrap
 import { connect } from 'react-redux';
 import { requestUserSession } from '../../../../shared/actions/authActions';
 
+import userService from '../../../../shared/services/userService';
+
 class userScreen extends React.Component {
     constructor(props) {
         super(props);
