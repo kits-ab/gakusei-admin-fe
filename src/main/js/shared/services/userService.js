e8188906ce0461b9a736808c02985d0e0d7477af
Doverstav <pontus.doverstav@kits.se>
2017-09-26 16:30:51 +0200

    Add service for fetching users

---
+++ src/main/js/shared/services/userService.js
@@ -0,0 +1,15 @@
+import 'whatwg-fetch';
+
+import getCSRF from '../util/CSRFToken';
+
+export default function userService() {
+    const urlUsers = 'api/users';
+
+    return {
+        get() {
+            return fetch(urlUsers, {
+                credentials: 'same-origin',
+            });
+        },
+    };
+}
