002176f32cffd2eb445646b6f453e1cfe5f3b117
Doverstav <pontus.doverstav@kits.se>
2017-09-27 11:11:43 +0200

    Implement getUsers()

--- src/main/js/screens/app/screens/users/userScreen.js
+++ src/main/js/screens/app/screens/users/userScreen.js
@@ -8,12 +8,47 @@ import userService from '../../../../shared/services/userService';
 class userScreen extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            users: [],
+        };
+
+        this.getUsers();
     }
         
     render() {
         return (
-            <h1> User go here! </h1>
+            <div>
+                { 
+                    this.state.users.map(user => (
+                        <h2> {user.username} </h2>
+                    ))
+                }
+            </div>
         );
+    }
+
+    getUsers() {
+        userService().get().then((response) => {
+            switch (response.status) {
+                case 200:
+                    response.text().then((text) => {
+                        try {
+                            const data = JSON.parse(text);
+                            this.setState({
+                                users: data,
+                            });
+                        } catch (err) {
+                            this.setState({
+                                users: [],
+                            });
+                        }
+                    });
+                    break;
+                default:
+                    throw new Error();
+            }
+        }).catch((err) => {});
     }
 }
 
