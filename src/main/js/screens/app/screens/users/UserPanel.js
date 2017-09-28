e719d5d196507e6cc54a5f0668e4af82e93bfa30
Doverstav <pontus.doverstav@kits.se>
2017-09-28 22:54:24 +0200

    Remove userPanel once user is deleted

--- src/main/js/screens/app/screens/users/UserPanel.js
+++ src/main/js/screens/app/screens/users/UserPanel.js
@@ -12,6 +12,7 @@ class UserPanel extends React.Component {
         this.state = {
             showModal: false,
             confirmDelete: false,
+            deleted: false,
         };
 
         this.closeModal = this.closeModal.bind(this);
@@ -22,29 +23,35 @@ class UserPanel extends React.Component {
     render() {
         return (
             <div>
-                <Panel header={userUtils().createHeader(this.props.user)} >
-                    <ButtonToolbar>
-                        <Button bsStyle='primary' onClick={this.openModal} > Show info </Button>
-                        <Button bsStyle='warning' > Reset password </Button>
-                        <Button bsStyle='danger' onClick={this.deleteUser} > {this.state.confirmDelete ? 'Confirm' : 'Delete'} </Button>
-                    </ButtonToolbar>
-                </Panel>
-                <Modal show={this.state.showModal} onHide={this.closeModal} bsSize="large" aria-labelledby="contained-modal-title-lg">
-                    <Modal.Header closeButton>
-                        <Modal.Title> {this.props.user.username} </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Panel>
-                            <h4> <strong> Username: </strong> {this.props.user.username} </h4>
-                            <h4> <strong> Role: </strong> {userUtils().stringifyRole(this.props.user.role)} </h4>
-                        </Panel>
-                        <ProgressTable progressList={this.props.user.progressTrackingList} />
-                        <EventTable events={this.props.user.events} />
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button onClick={this.closeModal} > Close </Button>
-                    </Modal.Footer>
-                </Modal>
+                {this.state.deleted ? 
+                <Panel header={`${this.props.user.username} was deleted`} bsStyle='info' /> 
+                :
+                <div>
+                    <Panel header={userUtils().createHeader(this.props.user)} >
+                        <ButtonToolbar>
+                            <Button bsStyle='primary' onClick={this.openModal} > Show info </Button>
+                            <Button bsStyle='warning' > Reset password </Button>
+                            <Button bsStyle='danger' onClick={this.deleteUser} > {this.state.confirmDelete ? 'Confirm' : 'Delete'} </Button>
+                        </ButtonToolbar>
+                    </Panel>
+                    <Modal show={this.state.showModal} onHide={this.closeModal} bsSize="large" aria-labelledby="contained-modal-title-lg">
+                        <Modal.Header closeButton>
+                            <Modal.Title> {this.props.user.username} </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Panel>
+                                <h4> <strong> Username: </strong> {this.props.user.username} </h4>
+                                <h4> <strong> Role: </strong> {userUtils().stringifyRole(this.props.user.role)} </h4>
+                            </Panel>
+                            <ProgressTable progressList={this.props.user.progressTrackingList} />
+                            <EventTable events={this.props.user.events} />
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={this.closeModal} > Close </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
+                }
             </div>
         );
     }
@@ -61,7 +68,7 @@ class UserPanel extends React.Component {
         if (!this.state.confirmDelete) {
             this.setState({ confirmDelete: true });
         } else {
-            window.alert(`${this.props.user.username} deleted!`);
+            this.setState({ deleted: true });
         }
     }
 }
