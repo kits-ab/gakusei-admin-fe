fcdbeaf9cf11a75fff1ce35b06a243a44f49fc71
Doverstav <pontus.doverstav@kits.se>
2017-09-28 20:44:13 +0200

    Add modal for showing user information

--- src/main/js/screens/app/screens/users/userScreen.js
+++ src/main/js/screens/app/screens/users/userScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Grid, Row, Col, ListGroup, ListGroupItem, Panel } from 'react-bootstrap';
+import { Alert, Grid, Row, Col, Form, FormGroup, FormControl, Button,
+    ButtonGroup, Panel, ButtonToolbar, Modal, Table } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { requestUserSession } from '../../../../shared/actions/authActions';
 
@@ -11,21 +12,124 @@ class userScreen extends React.Component {
 
         this.state = {
             users: [],
+            showModal: false,
         };
 
         this.getUsers();
+        this.closeModal = this.closeModal.bind(this);
+        this.openModal = this.openModal.bind(this);
+        // this.timestampToDate = this.timestampToDate.bind(this);
     }
         
     render() {
         return (
-            <div>
+            <Grid>
                 { 
                     this.state.users.map(user => (
-                        <h2> {user.username} </h2>
+                        <div>
+                            <Panel header={this.createHeader(user)} >
+                                <ButtonToolbar>
+                                    <Button bsStyle='primary' onClick={this.openModal} > Show info </Button>
+                                    <Button bsStyle='warning'> Reset password </Button>
+                                    <Button bsStyle='danger'> Delete </Button>
+                                </ButtonToolbar>
+                            </Panel>
+                            <Modal show={this.state.showModal} onHide={this.closeModal} bsSize="large" aria-labelledby="contained-modal-title-lg">
+                                <Modal.Header closeButton>
+                                    <Modal.Title> {user.username} </Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <p> Username: {user.username} </p>
+                                    <p> Role: {this.stringifyRole(user.role)} </p>
+                                    <Panel collapsible header='Progress'>
+                                        <Table striped bordered>
+                                            <thead>
+                                                <tr>
+                                                    <th>Id</th>
+                                                    <th>nuggetID</th>
+                                                    <th>Correct answers</th>
+                                                    <th>Incorrect answers</th>
+                                                    <th>Latest timestamp</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {user.progressTrackingList.map(progress => (
+                                                    <tr>
+                                                        <td> {progress.id} </td>
+                                                        <td> {progress.nuggetID} </td>
+                                                        <td> {progress.correctCount} </td>
+                                                        <td> {progress.incorrectCount} </td>
+                                                        <td> {this.timestampToDate(progress.latestTimestamp)} </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </Table>
+                                    </Panel>
+                                    <Panel collapsible header='Events'>
+                                        <Table striped bordered>
+                                            <thead>
+                                                <tr>
+                                                    <th>Id</th>
+                                                    <th>Timestamp</th>
+                                                    <th>Gamemode</th>
+                                                    <th>Type</th>
+                                                    <th>Data</th>
+                                                    <th>nuggetID</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {user.events.map(event => (
+                                                    <tr>
+                                                        <td> {event.id} </td>
+                                                        <td> {this.timestampToDate(event.timestamp)} </td>
+                                                        <td> {event.gamemode} </td>
+                                                        <td> {event.type} </td>
+                                                        <td> {event.data} </td>
+                                                        <td> {event.nuggetId} </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </Table>
+                                    </Panel>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button onClick={this.closeModal} > Close </Button>
+                                </Modal.Footer>
+                            </Modal>
+                        </div>
                     ))
                 }
-            </div>
+            
+            </Grid>
         );
+    }
+
+    timestampToDate(timestamp) {
+        let date = new Date(timestamp);
+        return date.toLocaleString('sv');
+    }
+
+    createHeader(user) {
+        return `${this.stringifyRole(user.role)} : ${user.username}`;
+    }
+
+    stringifyRole(role) {
+        switch (role) {
+            case 'ROLE_USER':
+                return 'User';
+            case 'ROLE_ADMIN':
+                return 'Admin';
+            default:
+                return 'Null';
+        }
+    }
+
+    openModal() {
+        this.setState({ showModal: true });
+    }
+
+    closeModal() {
+        this.setState({ showModal: false });
     }
 
     getUsers() {
