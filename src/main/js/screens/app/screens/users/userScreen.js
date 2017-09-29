a13613b7aacc4013b662f34f3ab8985cc85c397d
Doverstav <pontus.doverstav@kits.se>
2017-09-29 14:05:42 +0200

    Add form for searching for users

--- src/main/js/screens/app/screens/users/userScreen.js
+++ src/main/js/screens/app/screens/users/userScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from 'react-bootstrap';
+import { Grid, Form, FormControl, FormGroup, ControlLabel, HelpBlock, Button, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { requestUserSession } from '../../../../shared/actions/authActions';
 
@@ -12,19 +12,71 @@ class userScreen extends React.Component {
 
         this.state = {
             users: [],
+            searchString: '',
         };
 
         this.getUsers();
+
+        this.onSearchStringChange = this.onSearchStringChange.bind(this);
     }
         
     render() {
         return (
             <Grid>
+                <Form horizontal>
+                    <FormGroup controlId='formSearchName' >
+                        <Col componentClass={ControlLabel} sm={2} >
+                            Username
+                        </Col>
+                        <Col sm={10} >
+                            <FormControl type='text' name='search' placeholder='Username' onChange={this.onSearchStringChange} />
+                            <FormControl.Feedback />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={2} >
+                            Role
+                        </Col>
+                        <Col sm={10} >
+                            <FormControl type='text' name='roleSearch' placeholder='Role' />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col smOffset={2} sm={10} >
+                            <Button>
+                                Search
+                            </Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
                 {this.state.users.map(user => (
                         <UserPanel key={user.username} user={user} />
                 ))}
             </Grid>
         );
+    }
+
+    onSearchStringChange(event) {
+        this.setState({ searchString: event.target.value });
+    }
+
+    search(searchString) {
+        userService().search(searchString).then((response) => {
+            switch (response.status) {
+                case 200:
+                    response.text().then((text) => {
+                        try {
+                            const data = JSON.parse(text);
+                            window.console.log(data);
+                        } catch (err) {
+                            window.console.log(err);
+                        }
+                    });
+                    break;
+                default:
+                    throw new Error();
+            }
+        }).catch((err) => {});
     }
 
     getUsers() {
