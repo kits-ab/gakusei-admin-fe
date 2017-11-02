3032e5cb74ed5d27af34ad0cac5cb6f4f8ffab11
Doverstav <pontus.doverstav@kits.se>
2017-11-02 16:48:50 +0100

    Create WordPanel

---
+++ src/main/js/screens/app/screens/grammar/components/WordPanel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class WordPanel extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <p> Hello Word! </p>
+    );
+  }
+}
+
+export default WordPanel;
