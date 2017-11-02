305ce10275d7f5b255128febe1a0e7084dcd609d
Doverstav <pontus.doverstav@kits.se>
2017-11-02 16:48:39 +0100

    Create InflectionPanel

---
+++ src/main/js/screens/app/screens/grammar/components/InflectionPanel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class InflectionPanel extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <p> Hello Inflection! </p>
+    );
+  }
+}
+
+export default InflectionPanel;
