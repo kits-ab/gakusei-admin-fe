17547a30c832925ea490fd36c1d786f1e1f1d82d
Doverstav <pontus.doverstav@kits.se>
2017-11-02 16:15:06 +0100

    GrammarScreen fetches and displays all grammar lessons

--- src/main/js/screens/app/screens/grammar/grammarScreen.js
+++ src/main/js/screens/app/screens/grammar/grammarScreen.js
@@ -1,15 +1,44 @@
 import React from 'react';
+import { Grid } from 'react-bootstrap';
+
 import { connect } from 'react-redux';
 import { requestUserSession } from '../../../../shared/actions/authActions';
+import grammarService from '../../../../shared/services/grammarService';
+
+import GrammarPanel from './components/GrammarPanel';
 
 class grammarScreen extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      grammarLists: [],
+    };
+  }
+
+  componentWillMount = () => {
+    this.getGrammarLists();
+  }
+
+  getGrammarLists = () => {
+    grammarService().getAll().then((response) => {
+      if (response.status === 200) {
+        response.text().then((text) => {
+          this.setState({ grammarLists: JSON.parse(text) });
+        });
+      } else {
+        throw new Error();
+      }
+    });
   }
 
   render() {
     return (
-      <div> Hello wolrd! </div>
+      <Grid>
+        {this.state.grammarLists.map(grammarList => 
+          <GrammarPanel key={grammarList.lesson} grammarList={grammarList} />   
+        )}
+      </Grid>
     );
   }
 }
