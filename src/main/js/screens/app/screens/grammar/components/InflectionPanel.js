b054396aa6172d1e4df67b1d8e49cd2cba25a1bb
Doverstav <pontus.doverstav@kits.se>
2017-11-06 16:36:21 +0100

    List inflections in simple manner

--- src/main/js/screens/app/screens/grammar/components/InflectionPanel.js
+++ src/main/js/screens/app/screens/grammar/components/InflectionPanel.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
+import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
+
 class InflectionPanel extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  handlePanelClick = () => {
+    this.setState({ expanded: !this.state.expanded });
   }
 
   render() {
     return (
-      <p> Hello Inflection! </p>
+      <Panel collapsible header={'Böjningar'} expanded={this.state.expanded} onClick={this.handlePanelClick} >
+        <ListGroup>
+          {this.props.inflections.used.map(inflection => 
+            <ListGroupItem> {inflection} </ListGroupItem>
+          )}
+        </ListGroup>
+      </Panel>
     );
   }
 }
