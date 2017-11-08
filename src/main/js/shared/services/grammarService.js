440b2fed598e96a38afc5317deb6bda751fce359
Doverstav <pontus.doverstav@kits.se>
2017-11-08 15:09:35 +0100

    Add functions for creating and deleting inflections

--- src/main/js/shared/services/grammarService.js
+++ src/main/js/shared/services/grammarService.js
@@ -12,5 +12,31 @@ export default function grammarService() {
         credentials: 'same-origin',
       });
     },
+    createInflection(inflection) {
+      let inflectionUrl = baseUrl.concat('/inflection');
+
+      return fetch(inflectionUrl, {
+        method: 'PUT',
+        credentials: 'same-origin',
+        body: JSON.stringify(inflection),
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'X-XSRF-TOKEN': getCSRF(),
+        },
+      });
+    },
+    deleteInflection(inflection) {
+      let inflectionUrl = baseUrl.concat('/inflection');
+
+      return fetch(inflectionUrl, {
+        method: 'DELETE',
+        credentials: 'same-origin',
+        body: JSON.stringify(inflection),
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'X-XSRF-TOKEN': getCSRF(),
+        },
+      });
+    },
   };
 }
