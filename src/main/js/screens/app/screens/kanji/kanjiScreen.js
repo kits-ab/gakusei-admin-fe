5204e8bb5fafebc68aeb0f7e1dc26e65bc376b86
Doverstav <pontus.doverstav@kits.se>
2017-12-20 13:36:15 +0100

    Add searchform to kanji screen

--- src/main/js/screens/app/screens/kanji/kanjiScreen.js
+++ src/main/js/screens/app/screens/kanji/kanjiScreen.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import { Grid } from 'react-bootstrap';
+
+import bookService from '../../../../shared/services/bookService';
+import kanjiService from '../../../../shared/services/kanjiService';
+import Utility from '../../../../shared/util/Utility';
+
+import KanjiSearch from './components/KanjiSearch';
 
 class kanjiScreen extends React.Component {
-    render() {
-        return (<p>Hello!</p>);
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: [],
+      kanjis: [],
+      offset: 0,
+    };
+  }
+
+  componentWillMount = () => {
+    this.getBooks();
+  }
+
+  extractBookIds = bookNames => (
+    this.state.books.filter(book => bookNames.includes(book.title)).map(book => book.id)
+  )
+
+  searchNuggets = (swedish, books) => {
+    this.setState({ kanjis: [], offset: 0 }, this.getNuggets(swedish, books));
+  }
+
+  getNuggets = (swedish, books) => {
+    kanjiService().getKanji(swedish, this.extractBookIds(books), this.state.offset).then((response) => {
+      response.text().then((text) => {
+        let data = JSON.parse(text);
+        let kanjis = this.state.kanjis.concat(data.content);
+        window.console.log(kanjis);
+        this.setState({ kanjis });
+      });
+    });
+  }
+
+  getBooks = () => {
+    bookService().getAll().then((response) => {
+      if (response.status === 200) {
+        response.text().then((text) => {
+          this.setState({ books: JSON.parse(text).sort((a, b) => Utility.compareStringProperty(a, b, 'title')) });
+        }).catch((err) => {
+          this.setState({ error: 'Kunde inte hantera böcker' });
+        });
+      } else {
+        throw new Error();
+      }
+    }).catch((err) => {
+      this.setState({ error: 'Kunde inte hämta böcker' });
+    });
+  };
+
+  render() {
+    return (
+      <Grid>
+        <KanjiSearch books={this.state.books} search={this.searchNuggets} />
+      </Grid>
+    );
+  }
 }
 
 export default kanjiScreen;
