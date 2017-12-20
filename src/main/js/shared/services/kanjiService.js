1bf915f768421063f0701abdfeb4f682c63e12bd
Doverstav <pontus.doverstav@kits.se>
2017-12-20 13:35:37 +0100

    Add method for fetching kanjis

---
+++ src/main/js/shared/services/kanjiService.js
@@ -0,0 +1,22 @@
+import 'whatwg-fetch';
+
+import getCSRF from '../util/CSRFToken';
+
+export default function nuggetService() {
+    const baseUrl = '/api/kanjis';
+
+    return {
+        getKanji(swedish, bookIds, offset) {
+            let pageSize = '10';
+            let url = baseUrl.concat('?offset=', offset, '&pageSize=', pageSize, '&swedish=', swedish);
+            if (bookIds.length !== 0) {
+                url = url.concat('&bookIds=', bookIds);
+            }
+
+            return fetch(url, {
+                method: 'GET',
+                credentials: 'same-origin',
+            });
+        },
+    };
+}
