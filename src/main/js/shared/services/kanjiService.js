b7edba1eb9caf89e48560b77b04f9e4b585fcbbe
Doverstav <pontus.doverstav@kits.se>
2017-12-20 15:21:38 +0100

    Change indentation

--- src/main/js/shared/services/kanjiService.js
+++ src/main/js/shared/services/kanjiService.js
@@ -3,20 +3,20 @@ import 'whatwg-fetch';
 import getCSRF from '../util/CSRFToken';
 
 export default function nuggetService() {
-    const baseUrl = '/api/kanjis';
+  const baseUrl = '/api/kanjis';
 
-    return {
-        getKanji(swedish, bookIds, offset) {
-            let pageSize = '10';
-            let url = baseUrl.concat('?offset=', offset, '&pageSize=', pageSize, '&swedish=', swedish);
-            if (bookIds.length !== 0) {
-                url = url.concat('&bookIds=', bookIds);
-            }
+  return {
+    getKanji(swedish, bookIds, offset) {
+      let pageSize = '10';
+      let url = baseUrl.concat('?offset=', offset, '&pageSize=', pageSize, '&swedish=', swedish);
+      if (bookIds.length !== 0) {
+        url = url.concat('&bookIds=', bookIds);
+      }
 
-            return fetch(url, {
-                method: 'GET',
-                credentials: 'same-origin',
-            });
-        },
-    };
+      return fetch(url, {
+        method: 'GET',
+        credentials: 'same-origin',
+      });
+    },
+  };
 }
