b7ff537bc129551f88339b8252508f2484c27a80
Doverstav <pontus.doverstav@kits.se>
2017-12-20 16:29:10 +0100

    Create form

--- src/main/js/screens/app/screens/kanji/components/KanjiForm.js
+++ src/main/js/screens/app/screens/kanji/components/KanjiForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Panel, Button, Form, FormControl, FormGroup, Col, ControlLabel } from 'react-bootstrap';
+import { Panel, Button, Form, FormControl, FormGroup, Col, ControlLabel, Glyphicon } from 'react-bootstrap';
 
 class KanjiForm extends React.Component {
   constructor(props) {
@@ -7,7 +7,31 @@ class KanjiForm extends React.Component {
 
     this.state = {
       showCreate: false,
+      swedish: '',
+      english: '',
+      description: '',
+      kanji: '',
+      books: [],
     };
+  }
+
+  getSelected = options => (
+    Object.keys(options).filter(key => options[key].selected).reduce((acc, key) => {
+      acc.push(options[key].value);
+      return acc;
+    }, [])
+  );
+
+  onInputChange = (event) => {
+    let name = event.target.name;
+    let options = event.target.options;
+    let value = name === 'books' ? this.getSelected(options) : event.target.value;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: build kanji object, send to createKanji()
   }
 
   renderShowButton = () => (
@@ -18,7 +42,74 @@ class KanjiForm extends React.Component {
 
   renderForm = () => (
     <Panel bsStyle="primary" header={'Skapa kanji'} >
-      <p> Hello! </p>
+      <Form horizontal onSubmit={this.handleSubmit}>
+        <FormGroup>
+          <Col componentClass={ControlLabel} md={2}>
+            Beskrivning
+          </Col>
+          <Col md={10} >
+            <FormControl
+              onChange={event => this.onInputChange(event)}
+              type="text"
+              name="description"
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col componentClass={ControlLabel} md={2}>
+            Svenska
+          </Col>
+          <Col md={10} >
+            <FormControl
+              onChange={event => this.onInputChange(event)}
+              type="text"
+              name="swedish"
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col componentClass={ControlLabel} md={2}>
+            Engelska
+          </Col>
+          <Col md={10} >
+            <FormControl
+              onChange={event => this.onInputChange(event)}
+              type="text"
+              name="english"
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col componentClass={ControlLabel} md={2}>
+            Kanji
+          </Col>
+          <Col md={10} >
+            <FormControl
+              onChange={event => this.onInputChange(event)}
+              type="text"
+              name="kanji"
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col componentClass={ControlLabel} md={2}>
+            Svenska
+          </Col>
+          <Col md={10} >
+            <FormControl 
+              multiple
+              componentClass="select"
+              name="books"
+              onChange={event => this.onInputChange(event)}
+            >
+              {this.props.books.map((ref, index) => <option key={index} value={ref.title}>{ref.title}</option>)}
+            </FormControl>
+          </Col>
+        </FormGroup>
+        <Button bsStyle="primary" type="submit" className="pull-right" >
+          <Glyphicon glyph="search" /> Skapa kanji
+        </Button>
+      </Form>
     </Panel>
   )
 
